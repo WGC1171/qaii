69402201f43a1171591293de67c2f766a06b0fd1
李皓晨 <woceoylhc@163.com>
2018-08-06 18:19:34 +0800

    项目初始化

---
+++ src/main/java/com/qaii/domain/EmpAvatarinfo.java
@@ -0,0 +1,33 @@
+package com.qaii.domain;
+
+public class EmpAvatarinfo {
+    private Integer id;
+
+    private Integer uid;
+
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
